b48fed3a1e0ddb50ad211274674f60c4701d83e2
COLLAGE-TG <taiga.imai.0130@gmail.com>
2023-11-01 09:14:22 +0000

    mod

--- tracer/pin/try_gettrace_test1.sh
+++ tracer/pin/try_gettrace_test1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$PIN_ROOT/pin -t obj-intel64/champsim_tracer.so -o /home/funkytaiga/ChampSim-Ramulator/tmp_trace/try_print_function_test1.champsim -s 100000 -t 200000 -- ../../tmp_benchmark/./a.out
+$PIN_ROOT/pin -t obj-intel64/champsim_tracer.so -o /home/funkytaiga/ChampSim-Ramulator/tmp_trace/try_print_function_test1.champsim -s 100000 -t 1000000 -- ../../tmp_benchmark/./a.out

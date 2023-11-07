ba9da5f697fc32fc3fa4feb6643ff4237318ea26
COLLAGE-TG <taiga.imai.0130@gmail.com>
2023-11-07 00:30:47 +0000

    tmp save

--- tmp_benchmark/gc_exe_c.sh
+++ tmp_benchmark/gc_exe_c.sh
@@ -5,8 +5,8 @@
 # 実行例
 # bash gc_exe_c.sh {filename}
 
-GC_ROOT=/home/funkytaiga/tools/gc-8.2.2
-
+# GC_ROOT=/home/funkytaiga/tools/gc-8.2.2
+GC_ROOT=/home/funkytaiga/tools
 # GC_ROOT=/home/funkytaiga/ChampSim_Ramulator
-gcc -I${GC_ROOT}/include -L${GC_ROOT}/lib -Wl,-R${GC_ROOT}/lib ${1} -lgc -g -O0
+gcc -I${GC_ROOT}/include -L${GC_ROOT}/gc-8.2.2/.libs -Wl,-R${GC_ROOT}/gc-8.2.2/.libs ${1} -lgc -g -O0
 /usr/bin/time ./a.out
